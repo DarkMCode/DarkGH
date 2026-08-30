a6a70801bceeb7fbc04f13f2bc432834259a9d85
DarkMCode <darkmcode@gmail.com>
2026-08-30 11:51:26 +0200

    Add files via upload

--- js/sound-effects.js
+++ js/sound-effects.js
@@ -1,4 +1,4 @@
-import { getWaveform, drawWaveform } from "/DarkGH/js/waveForm.js";
+import { getWaveform, drawWaveform } from "/DarkGH/js/waveForm.js?module";
 
 
 fetch("/DarkGH/json/soundEffects.json")
